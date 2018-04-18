7ba408078760960ed425acfd71299d495500f44c
TRUONGNLNSE6132\truongnln <nltgiang@gmail.com>
2018-04-19 01:13:20 +0700

    fix issue when call login function

--- app/src/main/java/com/sfms/app/FeedbackApi.java
+++ app/src/main/java/com/sfms/app/FeedbackApi.java
@@ -12,6 +12,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Created by truongnln on 04/03/2018.
@@ -28,5 +29,5 @@ public interface FeedbackApi {
     Call<JsonObject> saveFeedback(@Header("username") String username, @Body RequestBody feedback);
 
     @POST("mobile/login")
-    Call<Boolean> login(@Path("username") String username, @Path("password") String password);
+    Call<Boolean> login(@Query("username") String username, @Query("password") String password);
 }
